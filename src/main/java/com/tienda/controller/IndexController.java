c65bd11898e139fa9dc9db01c46a4f76db317232
valechow <valechowseg@gmail.com>
2022-10-09 21:38:29 -0600

    Semana 4: Proyecto inicial del curso

---
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -0,0 +1,39 @@
+
+package com.tienda.controller;
+import dao.ClienteDao;
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.tienda.domain.Cliente;
+
+
+@Controller
+public class IndexController {
+    
+    @Autowired
+    private ClienteDao clienteDao;
+    
+    @GetMapping("/")
+    public String inicio(Model model) {
+        var texto = "Estamos en semana 4";
+        model.addAttribute("mensaje", texto);
+        
+        
+        /*Cliente cliente1 = new Cliente("Juan","Perez Moreno","jperez@gmail.com","25611818");
+        Cliente cliente2 = new Cliente("Pedro","Moreno Perez ","pmoreno@gmail.com","2545278");
+        Cliente cliente3 = new Cliente("Rebeca","Perez Contreras","rperez@gmail.com","22562222");
+        
+
+        var clientes = Arrays.asList(cliente1,cliente2,cliente3);*/
+        
+        var clientes = clienteDao.findAll();
+        
+        model.addAttribute("clientes",clientes);
+        
+        return "index";
+    }
+        
+            
+}
---
+++ src/main/java/com/tienda/domain/Cliente.java
@@ -0,0 +1,31 @@
+
+package com.tienda.domain;
+import javax.persistence.*;
+import lombok.Data;
+@Data
+@Entity
+@Table(name="cliente")
+public class Cliente implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idCliente;
+    
+    
+    private String nombre;
+    private String apellidos;
+    private String correo;
+    private String telefono;
+    
+    public Cliente() {
+    }
+
+    public Cliente(String nombre, String apellidos, String correo, String telefono) {
+         this.nombre = nombre;
+         this.apellidos = apellidos;
+         this.correo = correo;
+         this.telefono = telefono;
+     }
+}
